3497f31e3ddb135e7345c569515b36a51414d3bb
Paweł Wojciechowski <felixd@konopnickiej.com>
2020-04-25 19:52:19 +0200

    gem

--- lib/shoper.rb
+++ lib/shoper.rb
@@ -4,36 +4,84 @@ require "json"
 
 # Shoper REST API main class
 class Shoper
-  @conf = {}
-  @conf[:url] = ENV["SHOPER_URL"]
-  @conf[:login] = ENV["SHOPER_USER"]
-  @conf[:pass] = ENV["SHOPER_PASS"]
-  @conf[:access_token] = ""
-
-  def self.hi
-    puts "Hello world!"
+  def initialize(shoper_url, shoper_username, shoper_password)
+    @configuration = {
+      :shoper => {},
+    }
+    @configuration[:shoper][:url] = shoper_url
+    @configuration[:shoper][:api] = @configuration[:shoper][:url] + "/webapi/rest/"
+    @configuration[:shoper][:username] = shoper_username
+    @configuration[:shoper][:password] = shoper_password
+    self.get_token
   end
 
-  def auth_details
-    # ENV.fetch("HTTP_AUTH_USERNAME") + ":" + ENV.fetch("HTTP_AUTH_PASSWORD")
-    @conf[:login] + ":" + @conf[:pass]
-  end
-
-  def auth
-    auth = RestClient::Request.execute(
+  def get_token
+    r = RestClient::Request.execute(
       method: :post,
-      url: @conf[:url] + "/webapi/rest/auth",
-      headers: { "Authorization" => "Basic " + Base64::encode64(auth_details) },
+      url: @configuration[:shoper][:url] + "/webapi/rest/auth",
+      headers: {
+        "Authorization" => "Basic " + Base64::encode64(
+          @configuration[:shoper][:username] + ":" + @configuration[:shoper][:password]
+        ),
+      },
     )
+    j = JSON.parse(r)
+    @configuration[:shoper][:token] = j["access_token"]
+    @configuration[:shoper][:token_expires] = Time.now + j["expires_in"]
+    @configuration[:shoper][:token_type] = j["token_type"]
+  end
 
-    @conf[:access_token] = JSON.parse(auth)["access_token"]
+  # Shoper.resource(resource, action, params, id, data)
+  #  * get    -> /aboutpages/<id>
+  #  * update -> /aboutpages/<id>
+  #  * delete -> /aboutpages/<id>
+  #  * insert -> /aboutpages
+  #  * list   -> /aboutpages
+  def resource(resource, action, data = { :id => nil, :params => nil, :data => nil })
+    case action
+    when "get", "update", "delete"
+      endpoint = resource + "/" + data[:id]
+    when "insert", "list"
+      endpoint = resource
+    else
+      return { :error => true }
+    end
+
+    pp endpoint
+    # Check if Shoper Token is still valid
+    self.get_token if Time.now > @configuration[:shoper][:token_expires]
+
+    r = RestClient::Resource.new(@configuration[:shoper][:api] + endpoint,
+                                 :headers => {
+                                   :authorization => "Bearer " +
+                                                     @configuration[:shoper][:token],
+                                 })
+
+    case p[:action]
+    when "get"
+      res = r.get :params => data[:params]
+    when "update"
+      res = r.put data[:data].to_json, :content_type => :json
+    when "delete"
+      res = r.delete
+    when "insert"
+      puts "Dodawanie produktu"
+      pp data[:data].to_json
+      res = r.post data[:data].to_json, :content_type => :json
+    when "list"
+      res = r.get
+    else
+      return { :error => true }
+    end
+    pp JSON.parse(res)
+  end
+
+  def update_resource(resource)
+
+    #       # p u
+    #
   end
 end
-
-# api = RestClient::Resource.new(@conf[:url] + "/webapi/rest",
-#                                :headers => {
-#                                  :authorization => "Bearer " + @conf[:access_token],
-#                                })
 
 # res = api["products"].get :params => { :limit => 50 }
 # res = JSON.parse(res)
@@ -45,18 +93,7 @@ end
 #     if bc[product["ean"]] != nil && bc[product["ean"]] != product["code"]
 #       p "ID: " + product["product_id"] + " EAN: " + product["ean"] + " CODE: " + product["code"] + " NEW CODE: " + bc[product["ean"]]
 #       # Update product
-#       papi = RestClient::Resource.new(@conf[:url] + "/webapi/rest/products/" + product["product_id"],
-#                                       :headers => {
-#                                         :authorization => "Bearer " + @conf[:access_token],
-#                                       })
-
-#       # bc[product["ean"]]
-#       data = { "code" => bc[product["ean"]] }.to_json
-#       u = papi.put data, :content_type => :json
-#       # p u
 #     end
 #   }
 #   page += 1
 # end
-
-# webapi/rest/bulk
